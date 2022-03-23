ed6daec3b82f962ea480e96db752775cfe6c6072
Gustavo Henrique Bernardo <gustavobernardo094@gmail.com>
2022-03-23 12:40:29 -0300

    Adding square to the page
    modified the entire function and add the scares into the HTML
    
    when you hover the mouse over a square, the background color of it is
    changed to red

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const container = document.getElementById('container')
 const grid_container = document.getElementById('grid-container')
 
-const rows = 16
-console.log(rows)
+const square = 16
 
-for (let i = 0; i < rows; i++) {
-  const gridItem = document.createElement('div')
-  gridItem.classList.add('grid-item')
-  grid_container.appendChild(gridItem)
+createSquare(square)
+
+function createSquare(square) {
+  let totalSquare = square * square
+  for (let i = 0; i < totalSquare; i++) {
+    const gridItem = document.createElement('div')
+    gridItem.classList.add('grid-item')
+    grid_container.style.gridTemplateRows = `repeat(${square}, 1fr)`
+    grid_container.style.gridTemplateColumns = `repeat(${square}, 1fr)`
+    grid_container.insertAdjacentElement("beforeend", gridItem)
+    console.log(gridItem)
+    gridItem.addEventListener('mouseenter', () => changeColor(gridItem))
+  }
+}
+
+function changeColor(color) {
+  color.style.backgroundColor = 'red'
 }
