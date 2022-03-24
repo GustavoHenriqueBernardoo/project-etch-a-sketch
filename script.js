fa21698367181bc9911f43b28205d4998c0e97c5
Gustavo Henrique Bernardo <gustavobernardo094@gmail.com>
2022-03-24 09:17:12 -0300

    Inserted two new buttons using javascript
    One to erase the color and return to white
    
    And another one to delete all squares from the page

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const container = document.getElementById('container')
 const grid_container = document.getElementById('grid-container')
+const buttons_container = document.getElementById('buttons-container')
 
-const square = 16
+let square = 16
 
 createSquare(square)
 
@@ -13,11 +14,44 @@ function createSquare(square) {
     grid_container.style.gridTemplateRows = `repeat(${square}, 1fr)`
     grid_container.style.gridTemplateColumns = `repeat(${square}, 1fr)`
     grid_container.insertAdjacentElement("beforeend", gridItem)
-    console.log(gridItem)
+    // console.log(gridItem)
     gridItem.addEventListener('mouseenter', () => changeColor(gridItem))
   }
+  eraseSquares()
+  deleteSquares()
 }
 
 function changeColor(color) {
   color.style.backgroundColor = 'red'
 }
+
+// Getting all grid-item elements
+const gridItems = document.querySelectorAll('.grid-item')
+
+
+function eraseSquares() {
+  const cleanBtn = document.createElement('button')
+  cleanBtn.classList.add('erase')
+  cleanBtn.innerText = 'Erase'
+  cleanBtn.addEventListener('click', () => {
+    gridItems.forEach(grid => {
+      grid.style.backgroundColor = '#fff'
+    })
+  })
+  buttons_container.appendChild(cleanBtn)
+}
+function deleteSquares() {
+  const deleteBtn = document.createElement('button')
+  deleteBtn.classList.add('delete')
+  deleteBtn.innerText = 'Delete'
+  deleteBtn.addEventListener('click', () => {
+    gridItems.forEach(grid => {
+      grid.remove()
+      // grid_container.remove()
+    })
+  })
+  buttons_container.appendChild(deleteBtn)
+}
+
+
+console.log(square)
